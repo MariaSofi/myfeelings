f2899c75a567b78090e8cc12f13401cb085bba63
MariaSofi <maria.landavazo@potros.itson.edu.mx>
2021-05-02 21:21:09 -0700

    Primer commit

---
+++ app/src/main/java/com/example/myfeelings_landavazoaguilar/utilities/CustomBarDrawable.kt
@@ -0,0 +1,61 @@
+package com.example.myfeelings_landavazoaguilar.utilities
+
+import android.content.Context
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.example.myfeelings_landavazoaguilar.R
+
+class CustomBarDrawable: Drawable {
+
+    var coordenadas: RectF? = null
+    var context: Context? = null
+    var emocion: emociones? = null
+
+    constructor(context: Context, emocion: emociones){
+        this.context = context
+        this.emocion = emocion
+    }
+
+
+
+    override fun draw(p0: Canvas) {
+        val fondo: Paint = Paint()
+        fondo.style = Paint.Style.FILL
+        fondo.isAntiAlias = true
+        fondo.color = context?.resources?.getColor(R.color.gray) ?: R.color.gray
+        val ancho: Float = (p0.width - 10).toFloat()
+        val alto: Float = (p0.height - 10).toFloat()
+
+        coordenadas = RectF(0.0F, 0.0F, alto, ancho)
+
+        p0.drawRect(coordenadas!!, fondo)
+
+        if (this.emocion != null){
+            val porcentaje: Float = this.emocion!!.porcentaje * (p0.width - 10)/100
+            var coordenadas2 = RectF(0.0F, 0.0F, porcentaje, alto)
+
+            var seccion: Paint = Paint()
+            seccion.style = Paint.Style.FILL
+            seccion.isAntiAlias = true
+            seccion.color = ContextCompat.getColor(this.context!!, emocion!!.color)
+
+            p0.drawRect(coordenadas2!!, seccion)
+        }
+
+    }
+
+    override fun setAlpha(alpha: Int) {
+
+    }
+
+    override fun getOpacity(): Int {
+        return PixelFormat.OPAQUE
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/myfeelings_landavazoaguilar/utilities/emociones.kt
@@ -0,0 +1,8 @@
+package com.example.myfeelings_landavazoaguilar.utilities
+
+data class emociones (
+        var nombre: String,
+        var porcentaje:Float,
+        var color:Int,
+        var total:Float
+)
